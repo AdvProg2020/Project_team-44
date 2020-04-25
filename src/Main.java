ea52311503a283a64d8ecac596adc2b7d363e81f
dariush <dariush_amiri7@yahoo.com>
2020-04-25 20:00:48 +0430

    3334411

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ConsoleView.start();
         System.out.println("okkkk aliiiii  broo33oo");
-        System.out.println("dariush");
+        System.out.println("dariush1");
     }
 }
