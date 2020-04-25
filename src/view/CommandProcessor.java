8fc8f62d73567157d70cd0f6ccff7356640f33bf
moamdavoodi <58753725+moamdavoodi@users.noreply.github.com>
2020-04-25 12:29:52 +0430

    first commit: change buyer to purchaser

--- src/view/CommandProcessor.java
+++ src/view/CommandProcessor.java
@@ -10,7 +10,6 @@ public class CommandProcessor {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void runCommandProcessorByMenu() {
-
         Menu.setScanner(scanner);
         Menu currentMenu = MainMenu.getInstance();
         currentMenu.show();
