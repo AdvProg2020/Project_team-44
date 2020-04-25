07a86983908490262f8ca59305a81a1e2d898238
moamdavoodi <58753725+moamdavoodi@users.noreply.github.com>
2020-04-25 22:49:43 +0430

    add test

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -2,7 +2,7 @@ package controller;
 
 public abstract class LoginPageController {
     public static void processCreateAccount(String type, String username) {
-
+        System.out.println("test");
     }
 
     public static void processLogin(String username) {
