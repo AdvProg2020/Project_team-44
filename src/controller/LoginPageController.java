6c1e99a96844aaac0bfeb5650757675dd173ed49
moamdavoodi <58753725+moamdavoodi@users.noreply.github.com>
2020-05-15 16:13:09 +0430

    ////

--- src/controller/LoginPageController.java
+++ src/controller/LoginPageController.java
@@ -12,7 +12,7 @@ import model.requests.RequestForSeller;
 public abstract class LoginPageController {
     private static boolean isMainManagerRegistered;
     private static Account loggedInAccount;
-
+//salam salam sad ta salam
     public static boolean isIsMainManagerRegistered() {
         return isMainManagerRegistered;
     }
