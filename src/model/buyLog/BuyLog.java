041eaf875b15cd3506b084676013085120d0cb6b
dariusamiri <dariush_amiri7@yahoo.com>
2020-05-16 14:20:50 +0430

    add methods to buyLog sort class added

--- src/model/buyLog/BuyLog.java
+++ src/model/buyLog/BuyLog.java
@@ -2,6 +2,8 @@ package model.buyLog;
 
 import model.product.Product;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -13,7 +15,7 @@ public class BuyLog {
     private ArrayList<Product> allPurchasedProducts;
     private String sellerName;
     private BuyLogStatus status;
-    private static ArrayList<BuyLog> allLogs;
+    private static ArrayList<BuyLog> allBuyLogs;
 
     public BuyLog(String logID, Date date, int moneyPaid, int discountCodeAmountUsed, ArrayList<Product> allPurchasedProducts, String sellerName) {
         this.logID = logID;
@@ -22,7 +24,51 @@ public class BuyLog {
         this.discountCodeAmountUsed = discountCodeAmountUsed;
         this.allPurchasedProducts = allPurchasedProducts;
         this.sellerName = sellerName;
-        allLogs.add(this);
+        allBuyLogs.add(this);
     }
 
+    public String getLogID() {
+        return logID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getMoneyPaid() {
+        return moneyPaid;
+    }
+
+    public double getDiscountCodeAmountUsed() {
+        return discountCodeAmountUsed;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public BuyLogStatus getStatus() {
+        return status;
+    }
+
+    public static BuyLog getBuyLogById(String logId) {
+        for (BuyLog allBuyLog : allBuyLogs) {
+            if (allBuyLog.getLogID().equals(logId)) ;
+            return allBuyLog;
+        }
+        return null;
+    }
+    public ArrayList<String> getInfo(){
+        ArrayList<String> info = new ArrayList<>();
+        info.add(this.getLogID());
+        Date date = this.getDate();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(date);
+        info.add(strDate);
+        info.add(String.valueOf(this.getMoneyPaid()));
+        info.add(String.valueOf(this.getDiscountCodeAmountUsed()));
+        info.add(this.getSellerName());
+        info.add(this.getStatus().toString());
+        return info;
+    }
 }
