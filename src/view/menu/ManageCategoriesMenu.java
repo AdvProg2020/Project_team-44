6fdcda68ee663777d4bb16f572008d48ec77d5e7
Lenovo <amirabbasmohammadi@gmail.com>
2020-05-16 06:49:31 -0700

    complete category menu

--- src/view/menu/ManageCategoriesMenu.java
+++ src/view/menu/ManageCategoriesMenu.java
@@ -1,5 +1,8 @@
 package view.menu;
 
+import controller.ManagerAccountController;
+import exception.CategoryNotExistsException;
+import exception.RequestNotExistsException;
 import model.account.Account;
 
 import java.util.HashMap;
@@ -15,8 +18,8 @@ public class ManageCategoriesMenu extends Menu {
     }
 
     @Override
-    public void show() {
-
+    public void menuWork() {
+        ManagerAccountController.processManageCategories();
     }
 
     private Menu getEditCategoryMenu() {
@@ -30,6 +33,23 @@ public class ManageCategoriesMenu extends Menu {
             @Override
             public void execute() {
                 String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back"))
+                    this.backInExecute();
+                else if (input.equalsIgnoreCase("logout") && this.getCurrentUserLoggedIn() != null)
+                    this.logoutInExecute();
+                else if (!input.matches("edit \\w+"))
+                    this.invalidCommandInExecute();
+                else {
+                    String category = input.substring(5);
+                    try {
+                        ManagerAccountController.processEditCategoryEach(category);
+                        System.out.println("edit category successful");
+                    } catch (CategoryNotExistsException editCategoryError) {
+                        editCategoryError.getMessage();
+                        this.execute();
+                    }
+
+                }
             }
         };
     }
@@ -45,6 +65,23 @@ public class ManageCategoriesMenu extends Menu {
             @Override
             public void execute() {
                 String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back"))
+                    this.backInExecute();
+                else if (input.equalsIgnoreCase("logout") && this.getCurrentUserLoggedIn() != null)
+                    this.logoutInExecute();
+                else if (!input.matches("add \\w+"))
+                    this.invalidCommandInExecute();
+                else {
+                    String category = input.substring(4);
+                    try {
+                        ManagerAccountController.processAddCategoryEach(category);
+                        System.out.println("Add category successful");
+                    } catch (CategoryNotExistsException addCategoryError) {
+                        addCategoryError.getMessage();
+                        this.execute();
+                    }
+
+                }
             }
         };
     }
@@ -60,6 +97,23 @@ public class ManageCategoriesMenu extends Menu {
             @Override
             public void execute() {
                 String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back"))
+                    this.backInExecute();
+                else if (input.equalsIgnoreCase("logout") && this.getCurrentUserLoggedIn() != null)
+                    this.logoutInExecute();
+                else if (!input.matches("remove \\w+"))
+                    this.invalidCommandInExecute();
+                else {
+                    String category = input.substring(7);
+                    try {
+                        ManagerAccountController.processRemoveCategoryEach(category);
+                        System.out.println("remove category successful");
+                    } catch (CategoryNotExistsException removeCategoryError) {
+                        removeCategoryError.getMessage();
+                        this.execute();
+                    }
+
+                }
             }
         };
     }
