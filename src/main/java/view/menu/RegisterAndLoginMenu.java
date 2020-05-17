bc2e5b7b13e7ffe8f17e61d9ecd8a1bc7e663048
Lenovo <amirabbasmohammadi@gmail.com>
2020-05-17 13:58:38 -0700

    edit RegisterAndLoginMenu

--- src/main/java/view/menu/RegisterAndLoginMenu.java
+++ src/main/java/view/menu/RegisterAndLoginMenu.java
@@ -10,8 +10,52 @@ import java.util.HashMap;
 public class RegisterAndLoginMenu extends Menu {
     public RegisterAndLoginMenu(Menu parent) {
         super("Register And Login Menu", parent);
-        HashMap<Integer, Menu> submenus = new HashMap<Integer, Menu>();
+        HashMap<Integer, Menu> submenus = new HashMap<>();
+        submenus.put(1,getRegisterMenu());
+        submenus.put(2, getLoginMenu());
         this.setSubmenus(submenus);
+    }
+
+    private Menu getRegisterMenu() {
+        return new Menu("Register Menu", this) {
+            @Override
+            public void show() {
+                System.out.println(this.getName() + ":");
+                System.out.println("Please enter your type and userName");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back"))
+                    this.backInExecute();
+                else if (!input.matches("create account (manager|seller|purchaser) \\S+"))
+                    this.invalidCommandInExecute();
+                else {
+                    String type = input.split("\\s")[2];
+                    String userName = input.split("\\s")[3];
+                    System.out.println("Please enter your password");
+                    String passWord = scanner.nextLine();
+                    System.out.println("Please enter your name");
+                    String name = scanner.nextLine();
+                    if(!name.matches("([A-Z]|[a-z]) ([A-Z]|[a-z])")){
+
+                    }
+
+                    try {
+                        LoginPageController.processLogin(userName, passWord);
+                        System.out.println("login successful");
+                        this.execute();
+                    } catch (UsernameNotExistsException userNameError) {
+                        System.err.println(userNameError.getMessage());
+                        this.execute();
+                    } catch (WrongPasswordException passWordError) {
+                        System.err.println(passWordError.getMessage());
+                        this.execute();
+                    }
+                }
+            }
+        };
     }
 
     private Menu getLoginMenu() {
@@ -19,28 +63,29 @@ public class RegisterAndLoginMenu extends Menu {
             @Override
             public void show() {
                 System.out.println(this.getName() + ":");
-                System.out.println("Please enter your username and password");
+                System.out.println("Please enter your username");
             }
 
             @Override
             public void execute() {
-                String input1 = scanner.nextLine();
-                String passWord = scanner.nextLine();
-                if (input1.equalsIgnoreCase("back"))
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back"))
                     this.backInExecute();
-                else if (!input1.matches("login \\w+"))
+                else if (!input.matches("login \\w+"))
                     this.invalidCommandInExecute();
                 else {
-                    String userName = input1.substring(6);
+                    String userName = input.substring(6);
+                    System.out.println("Please enter your password");
+                    String passWord = scanner.nextLine();
                     try {
                         LoginPageController.processLogin(userName, passWord);
                         System.out.println("login successful");
                         this.execute();
                     } catch (UsernameNotExistsException userNameError) {
-                        System.out.println(userNameError.getMessage());
+                        System.err.println(userNameError.getMessage());
                         this.execute();
                     } catch (WrongPasswordException passWordError) {
-                        System.out.println(passWordError.getMessage());
+                        System.err.println(passWordError.getMessage());
                         this.execute();
                     }
                 }
