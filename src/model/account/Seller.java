5f6a23f676df2383f85e11e0b20981ec725be8a4
dariusamiri <dariush_amiri7@yahoo.com>
2020-05-17 13:53:50 +0430

    somethings was changed.

--- src/model/account/Seller.java
+++ src/model/account/Seller.java
@@ -14,13 +14,16 @@ import java.util.Date;
 
 public class Seller extends Account {
     private String companyName;
+    private String companyAddress;
+    private String companyTelephone;
     private ArrayList<Product> productsToSell;
     private ArrayList<Offer> offersList;
-    private ArrayList<String> companyInfo;
 
-    public Seller(String userName, String firstName, String lastName, String eMail, String telephoneNumber, String password, String companyName) {
+    public Seller(String userName, String firstName, String lastName, String eMail, String telephoneNumber, String password, String companyName, String companyAddress, String companyTelephone) {
         super(userName, firstName, lastName, eMail, telephoneNumber, password);
         this.companyName = companyName;
+        this.companyAddress = companyAddress;
+        this.companyTelephone = companyTelephone;
         this.productsToSell = new ArrayList<>();
     }
 
@@ -28,8 +31,24 @@ public class Seller extends Account {
         return companyName;
     }
 
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public String getCompanyTelephone() {
+        return companyTelephone;
+    }
+
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public void setCompanyTelephone(String companyTelephone) {
+        this.companyTelephone = companyTelephone;
     }
 
     public ArrayList<Product> getProductsToSell() {
@@ -65,8 +84,8 @@ public class Seller extends Account {
         productsToSell.remove(requestedProduct);
     }
 
-    public void addProductRequest(int requestId, Seller seller, String productID, Category category, String name, String companyName, int price, String explanationText) {
-        new RequestForAddProduct(requestId, seller, productID, category, name, companyName, price, explanationText);
+    public void addProductRequest(Seller seller, Category category, String name, String companyName, int price, String explanationText) {
+        new RequestForAddProduct(seller, category, name, companyName, price, explanationText);
     }
 
 
@@ -78,13 +97,27 @@ public class Seller extends Account {
         new RequestForEditOff(seller, offer, field, oldValue, newValue);
     }
 
-    public void addOfferRequest(Seller seller, String offerID, ArrayList<Product> productList, Date initialDate, Date finalDate, int discountPercentage, int requestId) {
-        new RequestForAddOff(seller, offerID, productList, initialDate, finalDate, discountPercentage, requestId);
+    public void addOfferRequest(Seller seller, ArrayList<Product> productList, Date initialDate, Date finalDate, int discountPercentage) {
+        new RequestForAddOff(seller, productList, initialDate, finalDate, discountPercentage);
     }
 
-//    public ArrayList<String> getCompanyInfo() {
-//
-//    }
+    public ArrayList<String> getCompanyInfo() {
+        ArrayList<String> companyInfo = new ArrayList<>();
+        companyInfo.add(this.getCompanyName());
+        companyInfo.add(this.getCompanyAddress());
+        companyInfo.add(this.getCompanyTelephone());
+        return companyInfo;
+    }
+
+    public void editCompanyInfo(String field, String newValue){
+        if (field.equalsIgnoreCase("companyName")){
+            this.setCompanyName(newValue);
+        }else if (field.equalsIgnoreCase("companyAddress")){
+            this.setCompanyAddress(newValue);
+        }else if (field.equalsIgnoreCase("companyTelephone")){
+            this.setCompanyTelephone(newValue);
+        }
+    }
 
     public ArrayList<String> getAllSellLogIds() {
         ArrayList<String> sellLogIds = new ArrayList<>();
@@ -95,14 +128,16 @@ public class Seller extends Account {
         }
         return sellLogIds;
     }
-    public ArrayList<String> getProductsToSellIds(){
+
+    public ArrayList<String> getProductsToSellIds() {
         ArrayList<String> productIds = new ArrayList<>();
         for (Product product : this.getProductsToSell()) {
             productIds.add(product.getProductID());
         }
         return productIds;
     }
-    public ArrayList<String> getOfferListIds(){
+
+    public ArrayList<String> getOfferListIds() {
         ArrayList<String> offerIds = new ArrayList<>();
         for (Offer offer : this.getOffersList()) {
             offerIds.add(offer.getOfferID());
