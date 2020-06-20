ed94c98a5ac4eae39fdf812fd3631c150c9696bf
dariusamiri <dariush_amiri7@yahoo.com>
2020-06-20 14:47:00 +0430

    Merge branch 'davoodTest' of https://github.com/AdvProg2020/Project_team-44
     Conflicts:
    	src/main/java/controller/ProductsPageController.java
    	src/main/java/controller/SellerAccountController.java
    	src/main/java/controller/ValidationController.java
    	target/classes/main/Main.class
    	target/classes/model/Category.class
    	target/classes/model/CodedDiscount.class
    	target/classes/model/Sort/Sort.class
    	target/classes/model/account/Account.class
    	target/classes/model/account/Manager.class
    	target/classes/model/account/Purchaser.class
    	target/classes/model/account/Seller.class
    	target/classes/model/filtering/FilteringStatus.class
    	target/classes/model/product/Product.class
    	target/classes/model/requests/RequestForAddOff.class
    	target/classes/model/requests/RequestForAddProduct.class
    	target/classes/model/requests/RequestForEditOff.class
    	target/classes/model/requests/RequestForEditProduct.class
    	target/classes/module-info.class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -65,16 +65,16 @@ public class Main extends Application {
         }
     }
 
-//    private static void reload() {
-//        Category.setAllCategories(new Json<Category>().getAllJson("src/main/resources/Categories", "category"));
-//        Product.setAllProducts(new Json<Product>().getAllJson("src/main/resources/Products", "product"));
-//        Manager.setAllManagers(new Json<Manager>().getAllJson("src/main/resources/Accounts/Managers", "manager"));
-//        Seller.setAllSeller(new Json<Seller>().getAllJson("src/main/resources/Accounts/Sellers", "seller"));
-//        Purchaser.setAllPurchaser(new Json<Purchaser>().getAllJson("src/main/resources/Accounts/Purchasers", "purchaser"));
-//        Account.getAllAccounts().addAll(Manager.getAllManagers());
-//        Account.getAllAccounts().addAll(Seller.getAllSeller());
-//        Account.getAllAccounts().addAll(Purchaser.getAllPurchaser());
-//        setCategoryParent(Category.getAllCategories());
-//        setProductCategory();
-//    }
+    private static void reload() {
+        Category.setAllCategories(new Json<Category>().getAllJson("src/main/resources/Categories", "category"));
+        Product.setAllProducts(new Json<Product>().getAllJson("src/main/resources/Products", "product"));
+        Manager.setAllManagers(new Json<Manager>().getAllJson("src/main/resources/Accounts/Managers", "manager"));
+        Seller.setAllSeller(new Json<Seller>().getAllJson("src/main/resources/Accounts/Sellers", "seller"));
+        Purchaser.setAllPurchaser(new Json<Purchaser>().getAllJson("src/main/resources/Accounts/Purchasers", "purchaser"));
+        Account.getAllAccounts().addAll(Manager.getAllManagers());
+        Account.getAllAccounts().addAll(Seller.getAllSeller());
+        Account.getAllAccounts().addAll(Purchaser.getAllPurchaser());
+        setCategoryParent(Category.getAllCategories());
+        setProductCategory();
+    }
 }
