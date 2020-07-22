d31fa6b776016b720dead0d02f46242b145e256b
moamdavoodi <58753725+moamdavoodi@users.noreply.github.com>
2020-07-22 06:03:00 +0430

    phase 3; start fresh

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -56,6 +56,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) throws IOException {
+        System.out.println("start fresh!");
         reload();
         new ShopServer();
 //        setMediaPlayer("The Swimmer.mp3");
